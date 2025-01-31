3a5fc2341c61a66ca0157601fd62fbedf849bced
khanali454 <khanandkhan061@gmail.com>
2025-01-31 13:54:55 +0500

    webhook creation

--- server/index.js
+++ server/index.js
@@ -30,10 +30,11 @@ app.post("/webhook", function (req, res) {
     AutomationModel.findOne({ board_id: `${boardId}`,columnId:`${columnId}`})
     .then((rep) => {
          console.log("rep : ", rep);
+         res.status(200).send(req.body);
     })
     .catch((err) => console.error("Query Error:", err));
    
-    res.status(200).send(req.body);
+   
 
 });
 app.post('/create/automation', (req, resp) => {
